c78a12e6c0ff563078cd48a8106c99ec5b558472
Tan24CapWorkspace <51941066+Tan24CapWorkspace@users.noreply.github.com>
2019-08-19 12:09:19 +0530

    Add files via upload

--- Promocode.java
+++ Promocode.java
@@ -1,15 +1,19 @@
 package com.cg.bean;
 
+import java.sql.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name="promocode")
 public class Promocode {
 	@Id
 	@GeneratedValue(strategy=GenerationType.SEQUENCE)
@@ -18,14 +22,65 @@ public class Promocode {
 	@Column(length=20)
 	private String promocode;
 	
+	private Date startdate;
+	
+	private Date enddate;
+	
+	private Double amount;
+	
+	
+	
 	public Promocode() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Promocode(String promocode) {
+	
+
+	public Promocode(String promocode, Date startdate, Date enddate, Double amount) {
 		super();
 		this.promocode = promocode;
+		this.startdate = startdate;
+		this.enddate = enddate;
+		this.amount = amount;
 	}
+
+
+
+	public Date getStartdate() {
+		return startdate;
+	}
+
+
+
+	public void setStartdate(Date startdate) {
+		this.startdate = startdate;
+	}
+
+
+
+	public Date getEnddate() {
+		return enddate;
+	}
+
+
+
+	public void setEnddate(Date enddate) {
+		this.enddate = enddate;
+	}
+
+
+
+	public Double getAmount() {
+		return amount;
+	}
+
+
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+
 
 	public Integer getPromocodeId() {
 		return promocodeId;
